40ae0776e7da848413e123d24de7b92cde4a9b57
Jonathan Liu <jonliu168@gmail.com>
2025-06-06 18:20:07 -0700

    so this might be a generational fumble

--- dendroSolution.py
+++
@@ -1,7 +0,0 @@
-import dendropy
-
-t1 = dendropy.Tree.get(path="data/phylotree/Hypocreales.newick", schema="newick")
-t2 = dendropy.Tree.get(path="=data/phylotree/Corynebacterium.newick",schema="newick")
-
-rf = t1.robinson_foulds_distance(t2)
-print(rf)
